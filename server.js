ae8adbab6813117c1fe42cc5f78584097f8f96a4
inknsharps <nchen25@gmail.com>
2021-06-06 17:14:42 -0400

    update for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const routes = require("./routes/api")
+const routes = require("./routes")
 
 const PORT = process.env.PORT ||  3000;
 
