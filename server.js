afb4914563f6b408179ae8567933cbad0e96d037
inknsharps <nchen25@gmail.com>
2021-06-06 17:05:31 -0400

    update for deployment:

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const routes = require("./routes/api")
 
 const PORT = process.env.PORT ||  3000;
 
@@ -24,7 +25,7 @@ mongoose.connect(
 );
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
